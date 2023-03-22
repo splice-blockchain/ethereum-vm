2c1c7b425bc9c32f33a79dd4e098412448fb73e3
Andrei Maiboroda <andrei@ethereum.org>
2023-03-22 14:55:39 +0100

    WIP Do not create account if CREATE3 execution ends with STOP

--- test/state/host.cpp
+++ test/state/host.cpp
@@ -253,6 +253,14 @@ evmc::Result Host::create(const evmc_message& msg) noexcept
     assert(gas_left >= 0);
 
     const bytes_view code{result.output_data, result.output_size};
+
+    assert(msg.kind != EVMC_CREATE3 || result.status_code != EVMC_SUCCESS || !code.empty());
+    //    if (msg.kind == EVMC_CREATE3 && result.status_code == EVMC_SUCCESS && code.empty())
+    //    {
+    //        m_state.get_accounts().erase(msg.recipient);
+    //        return result;
+    //    }
+
     if (m_rev >= EVMC_SPURIOUS_DRAGON && code.size() > max_code_size)
         return evmc::Result{EVMC_FAILURE};
 
