1dec83541ba429ceac939c85061d1f230cb5ad33
Andrei Maiboroda <andrei@ethereum.org>
2023-05-05 17:23:03 +0200

    WIP CREATE3 state transition tests

--- test/unittests/state_transition_create_test.cpp
+++ test/unittests/state_transition_create_test.cpp
@@ -32,3 +32,254 @@ TEST_F(state_transition, create_tx)
 
     expect.post[create_address].code = bytes{0xFE};
 }
+
+TEST_F(state_transition, create3_non_empty_auxdata)
+{
+    static constexpr auto create_address = 0x83b142ae499dd0f718b9fa4ac6dd152ad3e9376b_address;
+
+    rev = EVMC_PRAGUE;
+    const auto deploy_data = "abcdef"_hex;
+    const auto deploy_container = eof1_bytecode(bytecode(OP_INVALID), 0, deploy_data);
+
+    const auto init_code =
+        calldatacopy(0, 0, OP_CALLDATASIZE) + returncontract(0, 0, OP_CALLDATASIZE);
+    const auto init_container = eof1_bytecode(init_code, 3, {}, deploy_container);
+
+    const auto factory_code = calldatacopy(0, 0, OP_CALLDATASIZE) +
+                              create3().container(0).input(0, OP_CALLDATASIZE).salt(0xff) +
+                              ret_top();
+    const auto factory_container = eof1_bytecode(factory_code, 4, {}, init_container);
+
+    tx.to = To;
+
+    const auto aux_data = "aabbccddeeff"_hex;
+    tx.data = aux_data;
+
+    pre.insert(*tx.to, {.nonce = 1, .code = factory_container});
+
+    const auto expected_container = eof1_bytecode(bytecode(OP_INVALID), 0, deploy_data + aux_data);
+
+    expect.post[*tx.to].nonce = pre.get(*tx.to).nonce + 1;
+    expect.post[create_address].code = expected_container;
+    expect.post[create_address].nonce = 1;
+}
+
+TEST_F(state_transition, create3_revert_empty_returndata)
+{
+    rev = EVMC_PRAGUE;
+    const auto init_code = revert(0, 0);
+    const auto init_container = eof1_bytecode(init_code, 2);
+
+    const auto factory_code =
+        calldatacopy(0, 0, OP_CALLDATASIZE) +
+        sstore(0, create3().container(0).input(0, OP_CALLDATASIZE).salt(0xff)) +
+        sstore(1, OP_RETURNDATASIZE) + OP_STOP;
+    const auto factory_container = eof1_bytecode(factory_code, 4, {}, init_container);
+
+    tx.to = To;
+    pre.insert(*tx.to, {.nonce = 1, .code = factory_container});
+
+    expect.post[*tx.to].nonce = pre.get(*tx.to).nonce + 1;
+    expect.post[*tx.to].storage[0x00_bytes32] = 0x00_bytes32;
+    expect.post[*tx.to].storage[0x01_bytes32] = 0x00_bytes32;
+}
+
+TEST_F(state_transition, create3_revert_non_empty_returndata)
+{
+    rev = EVMC_PRAGUE;
+    const auto init_code = mstore8(0, 0xaa) + revert(0, 1);
+    const auto init_container = eof1_bytecode(init_code, 2);
+
+    const auto factory_code =
+        calldatacopy(0, 0, OP_CALLDATASIZE) +
+        sstore(0, create3().container(0).input(0, OP_CALLDATASIZE).salt(0xff)) +
+        sstore(1, OP_RETURNDATASIZE) + OP_STOP;
+    const auto factory_container = eof1_bytecode(factory_code, 4, {}, init_container);
+
+    tx.to = To;
+    pre.insert(*tx.to, {.nonce = 1, .code = factory_container});
+
+    expect.post[*tx.to].nonce = pre.get(*tx.to).nonce + 1;
+    expect.post[*tx.to].storage[0x00_bytes32] = 0x00_bytes32;
+    expect.post[*tx.to].storage[0x01_bytes32] = 0x01_bytes32;
+}
+
+TEST_F(state_transition, create3_invalid_initcontainer)
+{
+    rev = EVMC_PRAGUE;
+    const auto init_code = bytecode{Opcode{OP_PUSH0}};
+    const auto init_container = eof1_bytecode(init_code, 0);
+
+    const auto factory_code =
+        calldatacopy(0, 0, OP_CALLDATASIZE) +
+        sstore(0, create3().container(0).input(0, OP_CALLDATASIZE).salt(0xff)) + OP_STOP;
+    const auto factory_container = eof1_bytecode(factory_code, 4, {}, init_container);
+
+    tx.to = To;
+    pre.insert(*tx.to, {.nonce = 1, .code = factory_container});
+
+    expect.post[*tx.to].nonce = pre.get(*tx.to).nonce + 1;
+    expect.post[*tx.to].storage[0x00_bytes32] = 0x00_bytes32;
+}
+
+TEST_F(state_transition, create3_initcontainer_aborts)
+{
+    rev = EVMC_PRAGUE;
+    const auto init_code = bytecode{Opcode{OP_INVALID}};
+    const auto init_container = eof1_bytecode(init_code, 0);
+
+    const auto factory_code =
+        calldatacopy(0, 0, OP_CALLDATASIZE) +
+        sstore(0, create3().container(0).input(0, OP_CALLDATASIZE).salt(0xff)) + OP_STOP;
+    const auto factory_container = eof1_bytecode(factory_code, 4, {}, init_container);
+
+    tx.to = To;
+    pre.insert(*tx.to, {.nonce = 1, .code = factory_container});
+
+    expect.post[*tx.to].nonce = pre.get(*tx.to).nonce + 1;
+    expect.post[*tx.to].storage[0x00_bytes32] = 0x00_bytes32;
+}
+
+TEST_F(state_transition, create3_initcontainer_return)
+{
+    rev = EVMC_PRAGUE;
+    const auto init_code = bytecode{0xaa + ret_top()};
+    const auto init_container = eof1_bytecode(init_code, 2);
+
+    const auto factory_code =
+        calldatacopy(0, 0, OP_CALLDATASIZE) +
+        sstore(0, create3().container(0).input(0, OP_CALLDATASIZE).salt(0xff)) + OP_STOP;
+    const auto factory_container = eof1_bytecode(factory_code, 4, {}, init_container);
+
+    tx.to = To;
+    pre.insert(*tx.to, {.nonce = 1, .code = factory_container});
+
+    expect.post[*tx.to].nonce = pre.get(*tx.to).nonce + 1;
+    expect.post[*tx.to].storage[0x00_bytes32] = 0x00_bytes32;
+}
+
+TEST_F(state_transition, create3_initcontainer_stop)
+{
+    rev = EVMC_PRAGUE;
+    const auto init_code = bytecode{Opcode{OP_STOP}};
+    const auto init_container = eof1_bytecode(init_code, 2);
+
+    const auto factory_code =
+        calldatacopy(0, 0, OP_CALLDATASIZE) +
+        sstore(0, create3().container(0).input(0, OP_CALLDATASIZE).salt(0xff)) + OP_STOP;
+    const auto factory_container = eof1_bytecode(factory_code, 4, {}, init_container);
+
+    tx.to = To;
+    pre.insert(*tx.to, {.nonce = 1, .code = factory_container});
+
+    expect.post[*tx.to].nonce = pre.get(*tx.to).nonce + 1;
+    expect.post[*tx.to].storage[0x00_bytes32] = 0x00_bytes32;
+}
+
+TEST_F(state_transition, create3_invalid_deploy_container)
+{
+    rev = EVMC_PRAGUE;
+    const auto deploy_data = "abcdef"_hex;
+    const auto deploy_container = eof1_bytecode(bytecode{Opcode{OP_PUSH0}}, 0, deploy_data);
+
+    const auto init_code =
+        calldatacopy(0, 0, OP_CALLDATASIZE) + returncontract(0, 0, OP_CALLDATASIZE);
+    const auto init_container = eof1_bytecode(init_code, 3, {}, deploy_container);
+
+    const auto factory_code =
+        calldatacopy(0, 0, OP_CALLDATASIZE) +
+        sstore(0, create3().container(0).input(0, OP_CALLDATASIZE).salt(0xff)) + OP_STOP;
+    const auto factory_container = eof1_bytecode(factory_code, 4, {}, init_container);
+
+    tx.to = To;
+    pre.insert(*tx.to, {.nonce = 1, .code = factory_container});
+
+    expect.post[*tx.to].nonce = pre.get(*tx.to).nonce + 1;
+    expect.post[*tx.to].storage[0x00_bytes32] = 0x00_bytes32;
+}
+
+TEST_F(state_transition, create3_deploy_container_max_size)
+{
+    rev = EVMC_PRAGUE;
+    block.gas_limit = 10'000'000;
+    tx.gas_limit = block.gas_limit;
+    pre.get(tx.sender).balance = tx.gas_limit * tx.max_gas_price + tx.value + 1;
+
+    static constexpr auto create_address = 0x8698b6f77599cfbae959981ab5209e05807fbce3_address;
+
+    const auto eof_header_size = static_cast<int>(eof1_bytecode({OP_INVALID}).size() - 1);
+    const auto deploy_code = (0x5fff - eof_header_size) * bytecode{Opcode{OP_JUMPDEST}} + OP_STOP;
+    const auto deploy_container = eof1_bytecode(deploy_code, 0);
+
+    // no aux data
+    const auto init_code = returncontract(0, 0, 0);
+    const auto init_container = eof1_bytecode(init_code, 2, {}, deploy_container);
+
+    const auto factory_code =
+        calldatacopy(0, 0, OP_CALLDATASIZE) +
+        sstore(0, create3().container(0).input(0, OP_CALLDATASIZE).salt(0xff)) + OP_STOP;
+    const auto factory_container = eof1_bytecode(factory_code, 4, {}, init_container);
+
+    tx.to = To;
+    pre.insert(*tx.to, {.nonce = 1, .code = factory_container});
+
+    expect.post[*tx.to].nonce = pre.get(*tx.to).nonce + 1;
+    bytes32 create_address32{};
+    std::copy_n(create_address.bytes, sizeof(create_address), &create_address32.bytes[12]);
+    expect.post[*tx.to].storage[0x00_bytes32] = create_address32;
+    expect.post[create_address].code = deploy_container;
+}
+
+TEST_F(state_transition, create3_deploy_container_too_large)
+{
+    rev = EVMC_PRAGUE;
+    block.gas_limit = 10'000'000;
+    tx.gas_limit = block.gas_limit;
+    pre.get(tx.sender).balance = tx.gas_limit * tx.max_gas_price + tx.value + 1;
+
+    const auto eof_header_size = static_cast<int>(eof1_bytecode({OP_INVALID}).size() - 1);
+    const auto deploy_code = (0x6000 - eof_header_size) * bytecode{Opcode{OP_JUMPDEST}} + OP_STOP;
+    const auto deploy_container = eof1_bytecode(deploy_code, 0);
+
+    // no aux data
+    const auto init_code = returncontract(0, 0, 0);
+    const auto init_container = eof1_bytecode(init_code, 2, {}, deploy_container);
+
+    const auto factory_code =
+        calldatacopy(0, 0, OP_CALLDATASIZE) +
+        sstore(0, create3().container(0).input(0, OP_CALLDATASIZE).salt(0xff)) + OP_STOP;
+    const auto factory_container = eof1_bytecode(factory_code, 4, {}, init_container);
+
+    tx.to = To;
+    pre.insert(*tx.to, {.nonce = 1, .code = factory_container});
+
+    expect.post[*tx.to].nonce = pre.get(*tx.to).nonce + 1;
+    expect.post[*tx.to].storage[0x00_bytes32] = 0x00_bytes32;
+}
+
+TEST_F(state_transition, create3_deploy_container_with_aux_data_too_large)
+{
+    rev = EVMC_PRAGUE;
+    block.gas_limit = 10'000'000;
+    tx.gas_limit = block.gas_limit;
+    pre.get(tx.sender).balance = tx.gas_limit * tx.max_gas_price + tx.value + 1;
+
+    const auto eof_header_size = static_cast<int>(eof1_bytecode({OP_INVALID}).size() - 1);
+    const auto deploy_code = (0x5fff - eof_header_size) * bytecode{Opcode{OP_JUMPDEST}} + OP_STOP;
+    const auto deploy_container = eof1_bytecode(deploy_code, 0);
+
+    // 1 byte aux data
+    const auto init_code = returncontract(0, 0, 1);
+    const auto init_container = eof1_bytecode(init_code, 2, {}, deploy_container);
+
+    const auto factory_code =
+        calldatacopy(0, 0, OP_CALLDATASIZE) +
+        sstore(0, create3().container(0).input(0, OP_CALLDATASIZE).salt(0xff)) + OP_STOP;
+    const auto factory_container = eof1_bytecode(factory_code, 4, {}, init_container);
+
+    tx.to = To;
+    pre.insert(*tx.to, {.nonce = 1, .code = factory_container});
+
+    expect.post[*tx.to].nonce = pre.get(*tx.to).nonce + 1;
+    expect.post[*tx.to].storage[0x00_bytes32] = 0x00_bytes32;
+}
